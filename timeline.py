2334bd0b7ca07dd894e9fd14d31f1f352ae9d273
iamstizzle <thatguyjoshua@yahoo.com>
2015-09-09 16:15:05 -0500

    Class cleanup
    Slight modifications to the game class. Added a board sorting .

--- timeline.py
+++ timeline.py
@@ -2,6 +2,7 @@
 
 #---Setup---
 from random import randint
+import time
 #test# print(randint(1,20))
 from deck1 import history_deck 
 
@@ -9,10 +10,7 @@ class timeline():
         def __init__(self):
                 self.playertot = 0
                 self.deck = history_deck
-                self.player1 = []
-                self.player2 = []
-                self.player3 = []
-                self.player4 = []
+                self.board = []
                 self.randdraw = False
 
 
@@ -28,28 +26,40 @@ class timeline():
                 print("random number drawn",self.randdraw)
                 addtoplayer = self.deck[self.randdraw]
                 self.removefromdeck(self.randdraw)
-                
-                
-        
+                return addtoplayer
 
-        
+        def startboard(self):
+                self.randdraw = randint(0, len(self.deck)-1)
+                print("random number drawn",self.randdraw)
+                addtoboard = self.deck[self.randdraw]
+                self.removefromdeck(self.randdraw)
+                self.board.append(addtoboard)
+                
+                     
+        def sortboard(self):
+                self.board = sorted(self.board, key=lambda x: x[0])
+                #sort(self.board, lamba x: x, key=item[0])
+
+
+#sanity testing phase#
 newgame = timeline()
 newgame.playersetup()
 
 print(newgame.playertot)
 print(newgame.deck)
 print(len(newgame.deck))
-
-newgame.loadhand()
-print(len(newgame.deck))
-newgame.loadhand()
-print(len(newgame.deck))
+newgame.startboard()
+newgame.startboard()
+newgame.startboard()
+newgame.startboard()
+print("startingcard" , newgame.board)
 newgame.loadhand()
 print(len(newgame.deck))
 newgame.loadhand()
 print(len(newgame.deck))
 print(newgame.deck)
+newgame.sortboard()
+print(newgame.board)
 
-import time
 
 time.sleep(2)
