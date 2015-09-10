300008502f256fb27564abfb3fa6a673de728b0d
iamstizzle <thatguyjoshuaspam@yahoo.com>
2015-09-09 22:50:32 -0500

    Movie update
    new deck

--- deck1.py
+++ deck1.py
@@ -6,5 +6,37 @@ history_deck =[
     [1812, 'somewar'],
     [1690, 'potatofamine'],
     [1999, 'thematrixareforkids'],
-    [1983, 'coolguy']
+    [1983, 'Doctor J Stizzle was born']
+    [1865, 'Assassination of Abraham Lincoln'],
+    [1969, 'First Moon landing'],
+    [1803, 'The Louisiana Purchase'],
+    [1963, 'Assassination of John F. Kennedy'],
+    [1993, 'First World Trade Center Bombing attemp'],
+    [1879, 'Edison files patent for the first light bulb'],
     ]
+
+
+filmandculure_deck =[
+    [1983, 'Scarface (movie)'],
+    [1983, 'Flashdance (movie)'],
+    [1983, 'War Games (movie)'],
+    [1983, 'Star Wars: Return of the Jedi(movie)'],
+    [1983, 'A Christmas Story (movie)'],
+    [1983, 'Mr. Mom (movie)'],
+    [1983, 'Trading Places (movie)'],
+    [1983, "National Lampoon's Vacation(movie)"],
+    [1984, 'Ghostbusters (movie)'],
+    [1984, 'Gremlins (movie)'],
+    [1984, 'Indiana Jones and the Temple of Doom (movie)'],
+    [1984, 'The Karate Kid (movie)'],
+    [1984, 'Beverly Hills Cop (movie)'],
+    [1984, 'Terminator (movie)'],
+    [1984, 'Footloose (movie)'],
+    [1984, 'The NeverEnding Story (movie)'],
+    [1984, 'This Is Spinal Tap (movie)'],
+    [1988, 'BIG (Tom Hanks) (movie)'],
+    [1988, 'Beetlejuice (movie)'],
+    [1988, 'Die Hard (movie)'],
+    [1988, 'Bloodsport (movie)'],
+    ]
+    
