8c7aa1462159c2dc570e21783aebf7c975f24025
iamstizzle <thatguyjoshua@yahoo.com>
2015-09-17 12:45:59 -0500

    Mostly functional command prompt game
    Added player selections. Sanitized user inputs. Game flow should loop
    properly. Need to add more cards because deck empties at 4 players.

--- timeline.py
+++ timeline.py
@@ -6,21 +6,21 @@ import time
 from deck1 import filmandculture_deck
 
 
-
-##set game global variables ##
-deck = filmandculture_deck
-##################################
-##### functions ##################
+#######################
+##### functions #######
 def drawcard(whodraws):
         randdraw = randint(0, len(deck)-1)
         card_drawn = deck[randdraw]
         ##give it to the person/board
         whodraws.append(card_drawn)
         del deck[randdraw]
+        #removes the selection from the deck so it cannot be redrawn
+
 
 def showcards(cards):
         for each in cards:
                 print(str(each[0]) + " " + str(each[1]))
+
         
 def sortboard(board):
                 board = sorted(board, key=lambda x: x[0])
@@ -28,6 +28,7 @@ def sortboard(board):
                 #print(board)
                 #sort(self.board, lamba x: x, key=item[0])
                 return board
+
         
 def privateguess(board):
         secretresult = []
@@ -35,29 +36,36 @@ def privateguess(board):
                 secretresult.append(each)
         return secretresult
 
-#### need to debug ####  I believe it now fixed to return true and false but never none
+
 def validguess(placementguess2):
         guessvalid = True
         for each in range(0, len(placementguess2)-1):
                 if placementguess2[each][0] <= placementguess2[(each +1)][0]:
-                        print("\nChecking order.")
+                        print("\nChecking order.\n")
                         guessvalid = True
                 else:
-                        print("Incorrect guess")
+                        print("\nIncorrect guess\n")
                         guessvalid = False
-                        print("validguessoutput" ,validguess)
+                        #print("validguessoutput" ,validguess)
                         return guessvalid
         return guessvalid
-        print("validguessoutput" ,validguess)
 
-### need to debug ###
+
 def promptforyear(playerchoice, currentboard):
         count = 0
         tempguess = []
         ### added to avoid setting###
         for each in currentboard:
                 tempguess.append(each)
-        yearguess = int(raw_input("Guess the year the -- %s -- happened: " % playerchoice[1]))
+        yearguess = raw_input("Guess the year the -- %s -- happened: " % playerchoice[1])
+        ## sanitize for int
+        while yearguess.isdigit() == False:
+                time.sleep(1)
+                print("Please select a numeric value for the year")
+                yearguess = raw_input("Guess the year the -- %s -- happened: " % playerchoice[1])
+        # convert to int once proved valid
+        ### this system doesn't allow for negative i.e. B.C values. Need a better solution
+        yearguess = int(yearguess)
         for each in tempguess:
                 count +=1
                 if yearguess < each[0]:
@@ -66,19 +74,18 @@ def promptforyear(playerchoice, currentboard):
                 else:
                         pass
         tempguess.append(playerchoice)
-        print("sorry this was a higher guess than all\n")
+        #print("sorry this was a higher guess than all\n")
         return tempguess
 
 
-
-###--------------player setup-------------------------------###
+###--------------player setup------------------###
 
 class playerhand():
-        def __init__(self):
+        def __init__(self, name):
                 self.hand = []
-                self.name = "player"
+                self.name = name
                 self.cardchoice = False
-        def name(self):
+        def printname(self):
                 print(self.name)
                 
         #def drawcard(self, getcard):
@@ -94,94 +101,104 @@ class playerhand():
         def pickcard(self):
                 self.cardchoice = False
                 cardpick = raw_input("Which card do you choose: ")
+                #sanitize input to only accept int values within appropriate range
+                while cardpick.isdigit() == False or int(cardpick) > len(self.hand):
+                        print("Please choose a number between 1 and %s." % len(self.hand))
+                        cardpick = raw_input("Which card do you choose: ")                
                 self.cardchoice = self.hand[(int(cardpick) -1)]
                 choice = self.cardchoice
                 ## now remove that from hand.
                 del self.hand[(int(cardpick) -1)]
                 return choice
 
-
-
 #-----------------------------------
-######game test
-## board setup
 
-
-
-
-
-
-
-
-
-
+### TOTAL PLAYER SETUP
         
+totalplayers = raw_input("How many players? 2-4: " )
+while totalplayers.isdigit() ==False or int(totalplayers) > 4 or int(totalplayers) <2:
+        print("Please select an appropriate number.")
+        totalplayers = raw_input("How many players? 2-4: " )
+### raw_input makes inputs strings. you need to turn it back into an int)
+totalplayers = int(totalplayers)        
 
-### TODO: WORRY ABOUT SANITIZING INPUTS LATER
 
-
-totalplayers = 2 
 #deckselection = int(input("\nWhich deck would you like to play?\n1. History\n2. Art and culture\n3. Both\nChoice? : "))
 deck = filmandculture_deck
-### load hand with 5
-if totalplayers ==2:
-        gameboard = []
-        placementguess = []
-        drawcard(gameboard)
-        player1 = playerhand()
-        player2 = playerhand()
-        allplayers = [player1, player2]
-        placementguess = privateguess(gameboard)
-        placementguess = privateguess(gameboard)
-        placementguess = privateguess(gameboard)
-        placementguess = privateguess(gameboard)
-        print("@#$@#$@#$@#$@#$@#$@#$@#$@#$@#$#@$@#$#@$@$@#$@#$#@$", placementguess)
-        for each in allplayers:
-                count =0
-                while count <5:
-                        drawcard(each.hand)
-                        count +=1
-        print("\nPlayer 1 hand: \n")
-        player1.showhand()
-        print("\nPlayer 2 hand: \n")
-        player2.showhand()
-        print("\n\n")
-        #shoudl set up a victory condition loop
-        while len(player1.hand) > 0 and len(player2.hand) > 0 and len(deck) > 0:
-                #need to reset placementguess
-                placementguess = privateguess(gameboard)
-                for each in allplayers:
-                        ### safety reset
-                        placementguess = privateguess(gameboard)
-                        print("\n%s has the cards:\n" % each)
-                        each.showhand()
-                        print("\nThe current board shows:\n")
-                        showcards(gameboard)
-                        print('\n')
-                        #### whatever this function does it already makes gameboard = placementguess and that is bad
-                        placementguess = promptforyear(each.pickcard(), placementguess)
-                        print("@#$@#$#@$@#$#$@# leng -2 or more???", placementguess)
-                        #there may be in bug in validguess
-                        ### it makes it the same as bamgboard here and that is wrong.
-                        isvalid =  validguess(placementguess)
-                        print("whatdoesvalidguesssay  should be true or false never none,", isvalid)
-                        time.sleep(2)
-                        print(gameboard, "gameboard")
-                        if isvalid is True:
-                                gameboard = privateguess(placementguess)   # neve ever do i set the gameboard to == placementguess
-                                showcards(gameboard)
-                                #####showcards(placementguess)
-                                print("!!!! \n!!!!You Guessed Correctly!! temp commented out!!")
-                                time.sleep(1)
-                        else:
-                                print("\nThis is not correct. %s Drawing another card." % each)
-                                time.sleep(1)
-                                drawcard(each.hand)
-                                placementguess = privateguess(gameboard)
-                        
-                ### now start with player 1
-        print(player1.hand, player2.hand)
-print("\n\nsomeone won or the geck is out of cards")
-time.sleep(4)
+gameboard = []
+placementguess = []
+drawcard(gameboard)
+player1 = playerhand("Player 1")
+player2 = playerhand("Player 2")
+player3 = playerhand("Player 3")
+player4 = playerhand("Player 4")
+        
+### important to clone board to a backup value which can be checked###
+placementguess = privateguess(gameboard)
 
-                ##it only checks at the end of the turn so the game allows for ties if both players go out simultaneously.
+### setup game based on player selection
+if totalplayers == 2:
+        allplayers = [player1, player2]
+elif totalplayers == 3:
+                allplayers = [player1, player2, player3]
+elif totalplayers == 4:
+        allplayers = [player1, player2, player3, player4]
+else:
+        print("ERROR !!!! no players selected")
+
+
+########## START GAME ################
+
+##Gives players cards##   
+for each in allplayers:
+        count =0
+        while count <5:
+                drawcard(each.hand)
+                count +=1
+
+##Starts playing##
+while len(player1.hand) > 0 and len(player2.hand) > 0 and len(deck) > 0:
+                #need to reset placementguess
+        placementguess = privateguess(gameboard)
+        for each in allplayers:
+                        ### safety reset of placement
+                placementguess = privateguess(gameboard)
+                print("%s turn!" % each.name)
+                time.sleep(1)
+                print("\n%s has the cards:\n" % each.name)
+                each.showhand()
+                time.sleep(1)
+                print("\nThe current board shows:\n")
+                time.sleep(1)
+                print("------Game Board------")
+                showcards(gameboard)
+                print('\n')
+                ### Start guessing sequence ##
+                placementguess = promptforyear(each.pickcard(), placementguess)
+                isvalid =  validguess(placementguess)
+                #print("whatdoesvalidguesssay  should be true or false never none,", isvalid)
+                time.sleep(2)
+                ### Check if guess was valid ###
+                if isvalid is True:
+                        gameboard = privateguess(placementguess)
+                        showcards(gameboard)
+                        print("\n\nYou Guessed Correctly!\n\n")
+                        time.sleep(1)
+                else:
+                        print("\nThat is not a correct guess. %s Drawing another card.\n" % each.name)
+                        time.sleep(2)
+                        drawcard(each.hand)
+                        #resets the temporary guesses back to the gameboard since the temporary guesses were invalid.
+                        placementguess = privateguess(gameboard)
+                time.sleep(2)     
+                ### now start with player 1
+
+
+### while loop for game has exited. print victory conditions)
+                   ##it only checks at the end of the turn so the game allows for ties if both players go out simultaneously.
+                
+print("\n\nsomeone won or the deck is out of cards")
+
+time.sleep(4)
+             
+### known bugs.  it only places it relative to position so if there is an  an 83,83, ____ and you guess 83 and it is 84 it puts it at the end making you technically correct 83,83,__83__.
